11bf5a9f73070d5a3feb48d6eb71e0af603d3654
Luis Arrieta <luisarrieta796@gmail.com>
2024-05-09 18:17:42 -0500

    Check ports and use the first available port

--- exercises/chat-using-socketio/server/index.js
+++ exercises/chat-using-socketio/server/index.js
@@ -3,32 +3,57 @@ import logger from 'morgan'
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
 
-const port = process.env.PORT ?? 3000
+let selectedPort = null;
 
-const app = express()
-const server = createServer(app)
-const io = new Server(server, {
-  connectionStateRecovery: {}
-})
+const app = express();
+const io = new Server();
 
-io.on('connection', (socket) => {
-  console.log('An user has connected!')
+// Función para comprobar puertos disponibles
+const findAvailablePort = async () => {
+  for (let port = 3000; port <= 4000; port++) {
+    try {
+      await new Promise((resolve, reject) => {
+        const server = app.listen(port, () => {
+          server.close();
+          resolve(port);
+        });
+        server.on('error', reject);
+      });
+      selectedPort = port;
+      break;
+    } catch (err) {
+      console.log(`Port ${port} is in use.`);
+    }
+  }
+};
 
-  socket.on('disconnect', () => {
-    console.log('An user has disconnected')
+// Iniciar servidor
+const startServer = async () => {
+  await findAvailablePort();
+  const server = createServer(app);
+  io.attach(server);
+  
+  io.on('connection', (socket) => {
+    console.log('An user has connected!')
+
+    socket.on('disconnect', () => {
+      console.log('An user has disconnected')
+    })
+
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg)
+    })
   })
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg)
+  app.use(logger('dev'), express.static('client'))
+
+  app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + '/client/index.html')
   })
-})
 
-app.use(logger('dev'), express.static('client'))
+  server.listen(selectedPort, () => {
+    console.log(`Server running on port ${selectedPort}`)
+  })
+};
 
-app.get('/', (req, res)=>{
-  res.sendFile(process.cwd() + '/client/index.html')
-})
-
-server.listen(port, ()=>{
-  console.log(`Server running on port ${port}`)
-})
+startServer();

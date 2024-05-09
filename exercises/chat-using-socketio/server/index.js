2ae7f2d9fd114a2fe56a7f233e17c3b8a8175276
Luis Arrieta <luisarrieta796@gmail.com>
2024-05-09 17:02:00 -0500

    Emit messages from form to server

--- exercises/chat-using-socketio/server/index.js
+++ exercises/chat-using-socketio/server/index.js
@@ -12,8 +12,12 @@ const io = new Server(server)
 io.on('connection', (socket) => {
   console.log('An user has connected!')
 
-  socket.on('disconnect', ()=> {
+  socket.on('disconnect', () => {
     console.log('An user has disconnected')
+  })
+
+  socket.on('chat message', (msg) => {
+    console.log('message: '+msg)
   })
 })
 

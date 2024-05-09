8c332cdebf3173df3e0d7fe760a94e31b53606a5
Luis Arrieta <luisarrieta796@gmail.com>
2024-05-09 16:44:34 -0500

    Creating websocket server using socket.io

--- exercises/chat-using-socketio/server/index.js
+++ exercises/chat-using-socketio/server/index.js
@@ -1,15 +1,24 @@
 import express from 'express'
 import logger from 'morgan'
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
 
 const port = process.env.PORT ?? 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', () => {
+  console.log('A user has connected!')
+})
+
 app.use(logger('dev'))
 
 app.get('/', (req, res)=>{
   res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
   console.log(`Server running on port ${port}`)
 })

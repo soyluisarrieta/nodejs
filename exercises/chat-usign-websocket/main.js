d3dd927cfc00ad61a4220ac04d55861c04057388
Luis Arrieta <luisarrieta796@gmail.com>
2024-05-10 17:16:25 -0500

    Add dynamic port if a port is not available else find other

--- exercises/chat-usign-websocket/main.js
+++ exercises/chat-usign-websocket/main.js
@@ -1,34 +1,49 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
 const express = require('express');
 const WebSocket = require('ws');
+const path = require('path');
 const os = require('os');
 
 function getLocalIpAddress() {
-    const ifaces = os.networkInterfaces();
-    let ipAddress = 'localhost';
+  const ifaces = os.networkInterfaces();
+  let ipAddress = 'localhost';
 
-    Object.keys(ifaces).forEach(ifname => {
-        ifaces[ifname].forEach(iface => {
-            if (iface.family === 'IPv4' && !iface.internal) {
-                ipAddress = iface.address;
-            }
-        });
-    });
-
-    return ipAddress;
-}
-
-function createWindow() {
-  const mainWindow = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-          nodeIntegration: true
+  Object.keys(ifaces).forEach(ifname => {
+    ifaces[ifname].forEach(iface => {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        ipAddress = iface.address;
       }
+    });
   });
 
-  mainWindow.loadURL('http://' + getLocalIpAddress() + ':3000');
+  return ipAddress;
+}
+
+function findAvailablePort(startPort, endPort, callback) {
+  const net = require('net');
+  let port = startPort;
+
+  function checkPort(port) {
+    const server = net.createServer();
+    server.once('error', function (err) {
+      if (err.code === 'EADDRINUSE') {
+        // Puerto ocupado, probar el siguiente
+        server.close();
+        checkPort(port + 1);
+      } else {
+        // Otro error, notificar
+        callback(err);
+      }
+    });
+    server.once('listening', function () {
+      // Puerto disponible, cerrar el servidor y devolver el puerto
+      server.close();
+      callback(null, port);
+    });
+    server.listen(port, '0.0.0.0');
+  }
+
+  checkPort(port);
 }
 
 app.whenReady().then(() => {
@@ -36,39 +51,63 @@ app.whenReady().then(() => {
 
   // Servir la página principal del chat en la ruta raíz "/"
   server.get('/', (req, res) => {
-      res.sendFile(path.join(__dirname, 'client', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
   });
 
   // Servir archivos estáticos desde el directorio "public"
   server.use(express.static(path.join(__dirname, 'public')));
 
-  // Iniciar el servidor en el puerto 3000 solo en la dirección IP local
-  const expressServer = server.listen(3000, getLocalIpAddress(), () => {
-      console.log('Server running on http://' + getLocalIpAddress() + ':3000');
-  });
+  // Buscar un puerto disponible entre 3000 y 4000
+  findAvailablePort(3000, 4000, (err, port) => {
+    if (err) {
+      console.error('Error finding available port:', err);
+      return;
+    }
 
-  const wss = new WebSocket.Server({ server: expressServer });
+    // Configurar directorio de caché personalizado para Electron
+    app.commandLine.appendSwitch('disk-cache-dir', path.join(app.getPath('userData'), 'cache'));
 
-  wss.on('connection', function connection(ws) {
+    // Iniciar el servidor en el puerto disponible
+    const expressServer = server.listen(port, '0.0.0.0', () => {
+      console.log('Server running on http://' + getLocalIpAddress() + ':' + port);
+    });
+
+    const wss = new WebSocket.Server({ server: expressServer });
+
+    wss.on('connection', function connection(ws) {
       ws.on('message', function incoming(message) {
-          const parsedMessage = message.toString();
-          wss.clients.forEach(function each(client) {
-              if (client !== ws && client.readyState === WebSocket.OPEN) {
-                  client.send(parsedMessage);
-              }
-          });
+        const parsedMessage = message.toString();
+        wss.clients.forEach(function each(client) {
+          if (client !== ws && client.readyState === WebSocket.OPEN) {
+            client.send(parsedMessage);
+          }
+        });
       });
-  });
+    });
 
-  createWindow();
+    createWindow(port);
 
-  app.on('activate', function () {
-      if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    app.on('activate', function () {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow(port);
+    });
   });
 });
-
-
 
 app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit();
+  if (process.platform !== 'darwin') app.quit();
 });
+
+function createWindow(port) {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      webSecurity: false,
+      allowRunningInsecureContent: true,
+      partition: `persist:${port}`
+    }
+  });
+
+  mainWindow.loadURL('http://' + getLocalIpAddress() + ':' + port);
+}

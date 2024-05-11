5b9e188744a3a3dd1b482d3150f45263e1a6af1d
Luis Arrieta <luisarrieta796@gmail.com>
2024-05-11 12:42:40 -0500

    Clean main.js

--- exercises/03.chat-usign-socketio-and-electron/src/main.js
+++ exercises/03.chat-usign-socketio-and-electron/src/main.js
@@ -1,14 +1,12 @@
-const { app, BrowserWindow } = require('electron');
+const { app } = require('electron');
 const { createServer } = require('./server');
 
 // Iniciar la aplicación cuando esté lista
 app.whenReady().then(() => {
-  // Crear el servidor
   createServer();
 });
 
 // Manejar evento de cierre de todas las ventanas
 app.on('window-all-closed', () => {
-  // Cerrar la aplicación si no está en macOS
-  if (process.platform !== 'darwin') app.quit();
+  if (process.platform !== 'darwin') app.quit(); // Cerrar la aplicación si no está en macOS
 });
